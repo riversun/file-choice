6ae99b3c5411f9ab3f993969aa3aee9ca7183934
riversun <riversun.org@gmail.com>
2020-02-14 19:36:31 +0900

    update dependencies related event-emitter fix file-choice-integrated.test.js

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,13 @@ module.exports = {
         [
             '@babel/preset-env',
             {
-                'modules': 'umd',//false',//commonjs,amd,umd,systemjs,auto
-                'useBuiltIns': 'usage',
-                'targets': '> 0.25%, not dead',
-                'corejs': 3
+                'targets': {
+                    'browsers': [
+                        '>0.25%',
+                        'not ie 11',
+                        'not op_mini all'
+                    ]
+                }
             }
         ]
     ],
